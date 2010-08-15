62ee84b8a2aaa341b98cebe3b711fb25bc677cf3
Kevin Faustino <kevin.faustino@gmail.com>
2010-08-15 10:58:16 -0400

    Add geolocation query to groups

--- lib/meetup/group.rb
+++ lib/meetup/group.rb
@@ -1,13 +1,15 @@
 module Meetup
-  
+
   class Group
-    
+
     PATH = '/groups.json'
-    
-    def initialize(client, options={})      
+
+    def initialize(client, options={})
       @client = client
+      @options = options
+      @results = []
     end
-    
+
     def by_id(id)
       response = @client.connection.get do |request|
         request.url PATH, :id => id
@@ -15,7 +17,34 @@ module Meetup
       results = response.body.results
       results && results.length > 0 ? results.first : nil
     end
+
+    def geolocation(country, city, state)
+      @options[:country] = country
+      @options[:city] = city
+      @options[:state] = state
+      self
+    end
     
+    def limit(value)
+      @options[:page] = value.to_s
+      self
+    end
+    
+    def radius(value)
+      @options[:radius] = value.to_s
+      self
+    end
+
+    def fetch(force=false)
+      if @results.empty? || force
+        response = @client.connection.get do |request|
+          request.url PATH, @options
+        end
+        @results = response.body.results
+      end
+      @results
+    end
+
   end
-  
+
 end
--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -13,4 +13,22 @@ describe Meetup::Group do
     group.link.should == 'http://www.meetup.com/torontoruby/'
   end
 
+  it "should limit results by a geolocation" do
+    stub_request(:get, meetup_url(Meetup::Group::PATH)).with( :query => { :country => 'CA',
+                                                                          :city => 'Toronto',
+                                                                          :state => 'ON',
+                                                                          :page => '20',
+                                                                          :radius => '5.0',
+                                                                          :key => meetup_api_key}).to_return(:body => fixture_file('groups.json'), :status => 200)
+
+    groups = subject.geolocation('CA','Toronto','ON').radius(5.0).limit(20).fetch
+    groups.should have(20).items
+
+    groups.each do |group|
+      group.state.should == 'ON'
+      group.country.should == 'ca'
+      ['Toronto', 'North York'].should include(group.city)
+    end
+  end
+
 end
